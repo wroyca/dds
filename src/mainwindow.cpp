20ce658830d16714eae464b4ffc130e682b475f2
William Roy <wroy@protonmail.com>
2022-01-27 10:20:50 -0500

    Bad example of zydis decoding
    Signed-off-by: William Roy <wroy@protonmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,14 +4,69 @@
 #include "dia_interfaces.h"
 #include "dia_load_callback.h"
 
+#include <Zydis/Zydis.h>
+
 #include <QFileDialog>
 #include <QStandardPaths>
+#include <QTableWidgetItem>
+#include <QTreeWidgetItem>
+
+#include <cinttypes>
 
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
   , ui(new Ui::MainWindow), file_system_model(new QFileSystemModel(this))
 {
   ui->setupUi(this);
+
+  ZyanU8 data[] = {0x51, 0x8D, 0x45, 0xFF, 0x50, 0xFF, 0x75, 0x0C, 0xFF, 0x75, 0x08, 0xFF, 0x15, 0xA0, 0xA5, 0x48, 0x76, 0x85, 0xC0, 0x0F, 0x88, 0xFC, 0xDA, 0x02, 0x00};
+
+  // Initialize decoder context
+  ZydisDecoder decoder;
+  ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_STACK_WIDTH_64);
+
+  // Initialize formatter. Only required when you actually plan to do instruction
+  // formatting ("disassembling"), like we do here
+  ZydisFormatter formatter;
+  ZydisFormatterInit(&formatter, ZYDIS_FORMATTER_STYLE_INTEL);
+
+  // Loop over the instructions in our buffer.
+  // The runtime-address (instruction pointer) is chosen arbitrary here in order to better
+  // visualize relative addressing
+  ZyanU64 runtime_address = 0x007FFFFFFF400000;
+  ZyanUSize offset = 0;
+  constexpr auto length = sizeof(data);
+  ZydisDecodedInstruction instruction;
+  ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT_VISIBLE];
+
+  ui->treeWidget->setHeaderLabel("name");
+  ui->treeWidget->setColumnCount(2);
+  ui->treeWidget->setColumnWidth(1, 1);
+  //ui->tableWidget->insertRow(currentRowCount, 0, QTableWidgetItem("Some text"));
+
+
+  while (ZYAN_SUCCESS(ZydisDecoderDecodeFull(&decoder, data + offset, length - offset, &instruction, operands, ZYDIS_MAX_OPERAND_COUNT_VISIBLE, ZYDIS_DFLAG_VISIBLE_OPERANDS_ONLY)))
+  {
+    // Print current instruction pointer.
+    //printf("a %016" PRIX64 "  ", runtime_address);
+
+
+    // Format & print the binary instruction structure to human readable format
+    char buffer[256];
+    ZydisFormatterFormatInstruction(&formatter, &instruction, operands, instruction.operand_count_visible, buffer, sizeof(buffer), runtime_address);
+    puts(buffer);
+
+    QString a = buffer;
+
+    const auto item = new QTreeWidgetItem(); item->setText(0, a.section(' ', 0, 0));
+    ui->treeWidget->addTopLevelItem(item);
+
+    item->setText(1, a.section(' ', 1, 2));
+    ui->treeWidget->addTopLevelItem(item);
+
+    offset += instruction.length;
+    runtime_address += instruction.length;
+  }
 }
 
 MainWindow::~MainWindow()
